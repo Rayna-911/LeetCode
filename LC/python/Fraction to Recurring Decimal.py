1cc9d22da50f19dc00760f4ee2dbece3baa606a4
Rayna-911 <fuping911@hotmail.com>
2015-01-10 00:06:56 -0600

    LeetCode

--- LC/python/Fraction to Recurring Decimal.py
+++ LC/python/Fraction to Recurring Decimal.py
@@ -1,26 +1,42 @@
 class Solution:
     # @return a string
+    #when repeating in answer, mod repeats
     def fractionToDecimal(self, numerator, denominator):
-        if numerator%denominator == 0:
-            return str(numerator / denominator)[:-2]
         
-        ans = str(numerator / denominator)
-        ans=ans.split('.')
-        #print(len(ans[1]))
-        s = ''
-        if len(ans[1]) == 1:
-            if ans[1] == '0':
-                s= ''
-            else:
-                s = s+ans[1]
+        flag = 1
+        if numerator*denominator<0:
+            flag = -1
+            
+        num = abs(numerator)
+        den = abs(denominator)
+        
+        dict = {}
+        decimal = []
+        loop = None
+        
+        i=0
+        while True:
+            decimal.append(str(num/den))
+            i += 1
+            num = 10*(num%den)
+            
+            if num == 0:
+                break
+            
+            if num in dict:
+                start = dict.get(num)
+                loop = ''.join(decimal[start:i])
+                break
+            dict[num] = i
+        
+        res = decimal[0]
+        if len(decimal)>1:
+            res = res + '.'
+        if loop:
+            res = res + ''.join(decimal[1:len(decimal)-len(loop)]) + '(' + loop + ')'
         else:
-            first = ans[1][0]
-            if ans[1].count(first) == len(ans[1]):
-                s = s+'('+first+')'
-            else:
-                s = ans[1]
-        return ans[0]+'.'+s
-
-a = Solution()
-b = a.fractionToDecimal(1,5)
-print(b)
+            res = res + ''.join(decimal[1:])
+        if flag < 0:
+            res = '-'+ res
+        return res
+        
