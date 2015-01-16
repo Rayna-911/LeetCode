f5b1135854aca66d3d39b2fdf293187bb1bc8ec6
Rayna-911 <fuping911@hotmail.com>
2015-01-15 22:20:11 -0600

    LeetCode

--- LC/python/Longest Consecutive Sequence.py
+++ LC/python/Longest Consecutive Sequence.py
@@ -2,21 +2,21 @@ class Solution:
     # @param num, a list of integer
     # @return an integer
     def longestConsecutive(self, num):
-        n = []
-        for i in num:
-            if i not in n:
-                n.append(i)
-        print(n)
-        dict ={}
-        while n != []:
-            i = n.pop()
-            if (i - 1) in dict:
-                dict[i-1] += 1
-            elif (i + 1) in dict:
-                dict[i+1] += 1
-            else:
-                dict[i] = 1
-        return max(dict.values())
-
-a = Solution()
-print(a.longestConsecutive([1,0,-1]))
+        dict = {x:False for x in num}
+        m = -1
+        for i in dict:
+            if dict[i]==False:
+                curr = i-1
+                lenleft = 0
+                while curr in dict:
+                    lenleft += 1
+                    dict[curr] = True
+                    curr -= 1
+                curr = i+1
+                lenright = 0
+                while curr in dict:
+                    lenright += 1
+                    dict[curr] = True
+                    curr += 1
+                m = max(m,lenleft+lenright+1)
+        return m
